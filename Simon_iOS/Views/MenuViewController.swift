7b7e4dc1e2c6696d949f78e05ceb41e3caf8ce68
Thibma <balsamothibault@hotmail.fr>
2022-02-07 14:07:15 +0000

    init project

--- Simon_iOS/Views/MenuViewController.swift
+++ Simon_iOS/Views/MenuViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MenuViewController.swift
 //  Simon_iOS
 //
 //  Created by Thibault BALSAMO on 07/02/2022.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
 
 }
-
